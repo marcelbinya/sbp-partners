4dbc854d216de21d93a7366535f79d092a74dc38
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2020-12-19 04:54:22 +0100

    First iteration with grid and animations.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <main>
-    <header class="bg-white shadow" v-if="$route.meta.title">
+    <header class="bg-white shadow site-header" v-if="$route.meta.title">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">Smart Building Platform - Center Smart Commercial Building</h1>
       </div>
     </header>
 
-    <router-view />
+    <router-view class="router" />
   </main>
 </template>
+
+<style scoped>
+.router {
+  padding-top: 3rem;
+}
+
+.site-header {
+  position: fixed;
+  width: 100vw;
+  top: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,83 +1,183 @@
 <template>
   <main>
     <div class="bg-gray-50">
-<div class="container my-12 mx-auto px-4 md:px-12">
-    <div class="flex flex-wrap -mx-1 lg:-mx-4">
-        <!-- Column -->
-        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3" v-for="partner in partners" :key="partner.name">
-
+      <div class="bg-gray-200 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-4xl font-extralight view-header">
+          Integrierte Unternehmen, Komponenten und Produkte
+        </h1>
+      </div>
+      <div class="container my-12 mx-auto px-4 md:px-12">
+        <div class="flex flex-wrap -mx-1 lg:-mx-4 ">
+          <!-- Column -->
+          <div
+            class="my-1 px-1 w-full lg:my-4 lg:px-4 column"
+            :class="{
+              'lg:w-1/3 md:w-1/2': expandedPartner !== partner.name,
+              'hide': expandedPartner !== '' && expandedPartner !== partner.name
+            }"
+            v-for="partner in partners"
+            :key="partner.name"
+          >
             <!-- Article -->
             <article class="overflow-hidden rounded-lg shadow-lg">
+              <a href="#">
+                <img
+                  alt="Placeholder"
+                  class="block h-auto w-full"
+                  :src="partner.imageUrl"
+                />
+              </a>
 
-                <a href="#">
-                    <img alt="Placeholder" class="block h-auto w-full" :src="partner.imageUrl">
+              <header
+                class="flex items-center justify-between leading-tight p-2 md:p-4"
+              >
+                <h1 class="text-lg font-black">
+                  <a
+                    class="no-underline hover:underline text-black text-3xl font-mono"
+                    href="#"
+                  >
+                    {{ partner.name }}
+                  </a>
+                </h1>
+                <p class="text-grey-darker text-sm" @click="expand(partner)">Mehr anzeigen</p>
+              </header>
+
+              <div
+                class="flex items-center justify-between leading-none p-2 bg-gray-100"
+              >
+                <a class="flex items-center text-black space-x-1" href="#">
+                  <p
+                    class="text-sm"
+                    v-for="category in partner.categories"
+                    :key="category"
+                  >
+                    <span
+                      :class="categoryToTailwindClass(category)"
+                      class="rounded py-2 px-2 text-white font-semibold"
+                      >{{ category }}</span
+                    >
+                  </p>
                 </a>
+              </div>
 
-                <header class="flex items-center justify-between leading-tight p-2 md:p-4">
-                    <h1 class="text-lg underline">
-                        <a class="no-underline hover:underline text-black text-3xl " href="#">
-                            {{ partner.name }}
-                        </a>
-                    </h1>
-                    <p class="text-grey-darker text-sm">
-                        11/1/19
-                    </p>
-                </header>
-
-                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                    <a class="flex items-center no-underline text-black space-x-1" href="#">
-                        <!-- <img alt="Placeholder" class="block rounded-full" src="https://picsum.photos/32/32/?random"> -->
-                        <p class="text-sm" v-for="category in partner.categories" :key="category">
-                            <span :class="categoryToTailwindClass(category)" class="rounded py-2 px-4 text-white">{{ category }}</span>
-                        </p>
-                    </a>
-                    <!-- <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                        <span class="hidden">Like</span>
-                        <i class="fa fa-heart"></i>
-                    </a> -->
-                </footer>
-
+              <div class="p-2">
+                {{ partner.description }}
+              </div>
             </article>
             <!-- END Article -->
-
+          </div>
+          <!-- END Column -->
         </div>
-        <!-- END Column -->
-    </div>
-</div>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
-import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiAccount } from '@mdi/js'
- 
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiAccount } from "@mdi/js";
+
 export default {
-    components: {
-        SvgIcon
-    },
+  components: {
+    SvgIcon,
+  },
   data() {
     return {
+      expandedPartner: "",
       partners: [
-        { name: "Aedifion", categories: ["Software", "Hardware", "Gebäudeautomation", "Dashboards", "Platform"], imageUrl: 'https://picsum.photos/600/400/?random' },
-        { name: "w-tec", categories: ["Hardware", "Licht"], imageUrl: 'https://picsum.photos/600/400/?random' },
-        { name: "Zumtobel", categories: ["Hardware", "Licht"], imageUrl: 'https://picsum.photos/600/400/?random' }
+        {
+          name: "Aedifion",
+          categories: ["Gebäudeautomation", "Softwareplattform"],
+          description:
+            "aedifion.io ist die hochspezialisierte Cloud-Plattform für Monitoring und Optimierung von technischer Gebäudeausrüstung und Energiesystemen. Das Werkzeug, um Ihre Betriebskosten zu senken und Ihre Energieeffizienz zu steigern – für Bestand und Neubau gleichermaßen.",
+          imageUrl: "https://picsum.photos/600/100/?random",
+        },
+        {
+          name: "w-tec",
+          categories: ["Licht"],
+          description:
+            "Die smartengine-Technologie ist eine Infrastruktur für Gebäudeintelligenz und Lichtsteuerung. Herstellerneutrale LED-Leuchten werden mittels herkömmlicher Datenkabel mit Spannung versorgt und gleichzeitig multifunktionale Sensoren in diese Strom-Infrastruktur eingebunden. Die Sensordaten werden u.a. zur optimierten Steuerung von Licht, Heizung, Kühlung sowie Lüftung verwendet und liefern wertvolle Einblicke in die Gebäudenutzung.",
+          imageUrl: "https://picsum.photos/600/300/?random",
+        },
+        {
+          name: "Zumtobel",
+          categories: ["Licht"],
+          imageUrl: "https://picsum.photos/600/300/?random",
+        },
+        {
+          name: "Landmarken AG",
+          categories: ["Projektentwicklung"],
+          imageUrl: "https://picsum.photos/600/300/?random",
+        },
+        {
+          name: "w-tec",
+          categories: ["Leuchten", "Umweltsensorik"],
+          imageUrl: "https://picsum.photos/600/300/?random",
+        },
+        {
+          name: "Zumtobel",
+          categories: ["Leuchten", "Umweltsensorik"],
+          imageUrl: "https://picsum.photos/600/300/?random",
+        },
       ],
       path: mdiAccount,
-    }
+    };
   },
   methods: {
+    expand(partner) {
+      partner.expanded = !partner.expanded;
+      this.expandedPartner = this.expandedPartner.length === 0 ? partner.name : "";
+    },
     categoryToTailwindClass(category) {
-      switch(category) {
-        case "Software": return {
-          'bg-red-800': true
-        }
-        case "Hardware": return {
-          'bg-gray-700': true
-        }
+      switch (category) {
+        case "Gebäudeautomation":
+          return {
+            "bg-gray-700": true,
+            "mdi mdi-home-assistant": true,
+          };
+        case "Projektentwicklung":
+          return {
+            "bg-gray-300": true,
+            "text-gray-800": true,
+            "mdi mdi-home-city-outline": true,
+          };
+        case "Softwareplattform":
+          return {
+            "bg-gray-700": true,
+            "mdi mdi-application-cog": true,
+          };
+        case "Licht":
+          return {
+            "bg-gray-300": true,
+            "text-gray-800": true,
+            "mdi mdi-lightbulb-on": true,
+          };
       }
       return {};
-    }
-  }
-}
+    },
+  },
+};
 </script>
+
+<style scoped>
+.mdi::before {
+  padding-right: 0.2rem;
+}
+
+.view-header {
+  position: fixed;
+  width: 100vw;
+  height: 2.5rem;
+}
+
+.hide {
+  transform: translateY(100vw);
+}
+
+.column {
+  padding-top: 2rem;
+  transition: width 0.5s, height 0.2s, transform 1s, flex-grow 1s;
+}
+
+
+</style>
