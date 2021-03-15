bf6791058a9d376ecad1097ad5cb3e8cf406ff20
Jan Häusler <jan.haeusler@rwth-aachen.de>
2021-03-15 10:52:07 +0100

    Add esm bundle of axios.

--- src/views/Partner.vue
+++ src/views/Partner.vue
@@ -122,7 +122,7 @@ import SvgIcon from "@jamescoyle/vue-icon";
 import { mdiAccount } from "@mdi/js";
 import CardCollapsed from "./CardCollapsed.vue";
 import easyScroll from 'easy-scroll';
-import axios from 'axios';
+import { axios } from '@bundled-es-modules/axios';
 
 const lorem = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
 
